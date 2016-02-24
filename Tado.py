6ceba83beeb2182f910c5bec6ff69208939b190d
Chris Jewell <chrism0dwk@gmail.com>
2016-02-24 11:26:57 +0000

    Tidied Tado class a bit.  Removed refs to API v1.

--- Tado.py
+++ Tado.py
@@ -4,14 +4,22 @@ from cookielib import CookieJar
 import json
 
 class Tado:
-    """Interacts with a Tado thermostat via public API"""
+    """Interacts with a Tado thermostat via public API.
+    Example usage: t = Tado('me@somewhere.com', 'mypasswd')
+                   t.getClimate(1) # Get climate, zone 1.
+    """
 
+    # Class-wide constant info
     headers = { 'Referer' : 'https://my.tado.com/' }
-    cj = CookieJar()
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj),urllib2.HTTPSHandler())
     api2url = 'https://my.tado.com/api/v2/homes/'
 
-    def _api2Call(self, cmd):
+    # HTTPS Interface
+    cj = CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj),
+                                  urllib2.HTTPSHandler())
+
+    # 'Private' methods for use in class, Tado API V2.
+    def _apiCall(self, cmd):
         url = '%s%i/%s' % (self.api2url, self.id, cmd)
         req = urllib2.Request(url, headers=self.headers)
         response = self.opener.open(req)
@@ -22,7 +30,7 @@ class Tado:
         access_token = data['access_token']
         self.headers['Authorization'] = 'Bearer ' + access_token
         
-    def loginV2(self, username, password):
+    def _loginV2(self, username, password):
         url='https://my.tado.com/oauth/token'
         data = { 'client_id' : 'tado-webapp',
                  'grant_type' : 'password',
@@ -35,9 +43,11 @@ class Tado:
         response = self.opener.open(req)
         self._setOAuthHeader(json.loads(response.read()))
         return response
+
     
+    # Public interface
     def getMe(self):
-        """Gets home information"""
+        """Gets home information."""
         url = 'https://my.tado.com/api/v2/me'
         req = urllib2.Request(url, headers=self.headers)
         response = self.opener.open(req)
@@ -47,19 +57,14 @@ class Tado:
     def getState(self, zone):
         """Gets current state of Zone zone."""
         cmd = 'zones/%i/state' % zone
-        data = self._api2Call(cmd)
+        data = self._apiCall(cmd)
         return data
     
     def getCapabilities(self, zone):
         """Gets current capabilities of Zone zone."""
         cmd = 'zones/%i/capabilites' % zone
-        data = self._api2Call(cmd)
+        data = self._apiCall(cmd)
         return data
-    
-    def __init__(self, username, password):
-        """Performs login and save session cookie."""
-        self.loginV2(username, password)
-        self.id = self.getMe()['homes'][0]['id']
 
     def getClimate(self, zone):
         """Gets temp (centigrade) and humidity (% RH) for Zone zone."""
@@ -67,4 +72,9 @@ class Tado:
         data = self.getState(zone)['sensorDataPoints']
         return { 'temperature' : data['insideTemperature']['celsius'],
                  'humidity'    : data['humidity']['percentage'] }
-        
+
+    # Ctor
+    def __init__(self, username, password):
+        """Performs login and save session cookie."""
+        self._loginV2(username, password)
+        self.id = self.getMe()['homes'][0]['id']
