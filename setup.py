48137f10bc443648153b623aa4305c273ab84a6b
Wolfgang Malgadey <w.malgadey@cp-pro.de>
2016-12-31 11:22:25 +0100

    created setup file for module release

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='PyTado',
+      version='0.1.0',
+      description='PyTado from chrism0dwk',
+      keywords='tado',
+      author='chrism0dwk',
+      author_email='chrism0dwk@gmail.com',
+      url='https://github.com/chrism0dwk/PyTado',
+      zip_safe=False,
+      platforms=["any"],
+      packages=find_packages(),
+      )
