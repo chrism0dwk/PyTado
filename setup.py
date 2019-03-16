5f94c6dcc984e341fda925118332ddc06d2b1bc3
Wolfgang Malgadey <w.malgadey@cp-pro.de>
2019-03-16 11:43:12 +0100

    changed version to 0.2.8

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ readme = open(here('README.md')).read()
 requirements = [x.strip() for x in open(here('requirements.txt')).readlines()]
 
 setup(name='python-tado',
-      version='0.2.7',
+      version='0.2.8',
       description='PyTado from chrism0dwk, modfied by w.malgadey',
       long_description=readme,
       keywords='tado',
